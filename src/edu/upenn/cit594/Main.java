0728f4666971e072c151af27e8dc1853abd7ab79
jasmine <yuejian@seas.upenn.edu>
2019-12-01 17:11:21 -0800

    Add Main

---
+++ src/edu/upenn/cit594/Main.java
@@ -0,0 +1,8 @@
+package edu.upenn.cit594;
+/* 1. reading the runtime arguments / handle input through command
+ * 2. creating all the objects, arranging the dependencies between modules (“Monolith vs. Modularity”)
+ */
+
+public class Main {
+
+}
