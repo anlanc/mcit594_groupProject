031b0ff4f9672d43de90aa8dbee166a22e1ce182
AC <49619135+anlanc@users.noreply.github.com>
2019-12-03 13:53:56 -0800

    Add files via upload

---
+++ src/Population.java
@@ -0,0 +1,20 @@
+
+public class Population {
+	public int zipCode;
+	public int population;
+	public Population(int zipCode, int population) {
+		this.zipCode = zipCode;
+		this.population = population;
+	}
+	public int getZipCode() {
+		return zipCode;
+	}
+	public int getPopulation() {
+		return population;
+	}
+	@Override
+	public String toString() {
+		return zipCode + " " + population;
+	}
+	
+}
